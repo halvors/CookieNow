0dff78dd2c0fac5e1dc536a565f6a86eceaf072a
Winterforce <brage.skjonborg@hotmail.no>
2014-05-08 20:00:22 +0200

    the edit

---
+++ pages/FAQ.php
@@ -0,0 +1,19 @@
+<?php
+
+echo '<h1>Question</h1>';
+
+echo '<p>My computer won´t start..</p>';
+echo '<p>I just got back from my vaccation and now my computer won´t start..</p>';
+echo '<p>I keep hitting the ower button but it just wont start</p>';
+echo '<p>Any help out there</p>';
+
+echo '<p>-------------------------------------------------------------------------------</p>';
+
+echo '<h2>Answer</h2>';
+
+echo '<p>Have you tried plugging in the power cable?</p>';
+echo '<p></p>';
+
+
+
+?>
